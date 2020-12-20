1ff90f187a80f4b06672929b875d895e8253835c
junex10 <joaealejunior@gmail.com>
2020-12-20 16:23:57 -0400

    bot

--- sunat.py
+++ sunat.py
@@ -18,34 +18,51 @@ options.add_argument("no-sandbox")
 browser = webdriver.Chrome()
 browser.get(url)
 
-time.sleep(10)
+x = 0
+
+while x != 1:
+
+    html = browser.page_source
+    soup = BeautifulSoup(html, "html.parser")
+
+    com = soup.find_all('div', {"class": "normal-all-day"})
+
+    if len(com) > 0:
+        x = 1
+    else:
+        browser.refresh()
+        time.sleep(10)
 
 html = browser.page_source
 soup = BeautifulSoup(html, "html.parser")
 
-info = soup.find_all("td", {"class": "calendar-day"})
-
 datos = []
+
+info = soup.find_all("td", {"class": "calendar-day"})
 
 for i in info:
     fechas = i['data-date']
     tmp = []
-    
     compras = i.find_all('div', {"class": "normal-all-day"})
     ventas = i.find_all('div', {"class": "pap-all-day"})
 
+    print(compras)
+    #if len(compras) > 0:
+        
     tmp.append({"fecha": fechas})
 
     for c in compras:
         compra = re.search('[0-9]', c.text)
         compra = compra.string.replace('Compra ', '')
         tmp.append({"compra": compra})
-    
+        
     for c in ventas:
         venta = re.search('[0-9]', c.text)
         venta = venta.string.replace('Venta ', '')
         tmp.append({"venta": venta})
-    
+        
     datos.append(tmp)
+    #else:
+    #    browser.refresh()   
 
 print(datos)
