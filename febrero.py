5afd19f9738cb7a9717a5c9d6a2032aee0992594
junex10 <joaealejunior@gmail.com>
2020-12-28 20:05:14 -0400

    scrap

---
+++ febrero.py
@@ -0,0 +1,166 @@
+import time
+import re
+import urllib.request
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from webdriver_manager.chrome import ChromeDriverManager
+from bs4 import BeautifulSoup
+from selenium.webdriver.chrome.options import Options
+
+driver_exe = 'chromedriver'
+options = Options()
+options.add_argument("--headless") 
+
+browser = webdriver.Chrome(driver_exe, options=options)
+url = 'https://e-consulta.sunat.gob.pe/cl-at-ittipcam/tcS01Alias'
+browser.get(url)
+
+datos = []
+
+febrero = []
+
+def scrap(info, mes, varmes):
+
+    for i in info:
+        fechas = i['data-date']
+        tmp = []
+        compras = i.find_all('div', {"class": "normal-all-day"})
+        ventas = i.find_all('div', {"class": "pap-all-day"})
+            
+        tmp.append({"fecha": fechas})
+
+        for c in compras:
+            compra = re.search('[0-9]', c.text)
+            compra = compra.string.replace('Compra ', '')
+            tmp.append({"compra": compra})
+            
+        for c in ventas:
+            venta = re.search('[0-9]', c.text)
+            venta = venta.string.replace('Venta ', '')
+            tmp.append({"venta": venta})
+            
+        varmes.append(tmp) 
+
+    datos.append({mes: varmes})
+
+x = 0
+
+while x != 1:
+
+    html = browser.page_source
+    soup = BeautifulSoup(html, "html.parser")
+
+    com = soup.find_all('div', {"class": "normal-all-day"})
+
+    if len(com) > 0:
+        x = 1
+    else:
+        browser.refresh()
+        time.sleep(10)
+
+link = browser.find_elements_by_class_name("js-cal-prev")[0]
+link.click()
+time.sleep(5)
+
+link = browser.find_elements_by_class_name("js-cal-prev")[0]
+link.click()
+time.sleep(5)
+
+link = browser.find_elements_by_class_name("js-cal-prev")[0]
+link.click()
+time.sleep(5)
+
+link = browser.find_elements_by_class_name("js-cal-prev")[0]
+link.click()
+time.sleep(5)
+
+link = browser.find_elements_by_class_name("js-cal-prev")[0]
+link.click()
+time.sleep(5)
+
+link = browser.find_elements_by_class_name("js-cal-prev")[0]
+link.click()
+time.sleep(5)
+
+link = browser.find_elements_by_class_name("js-cal-prev")[0]
+link.click()
+time.sleep(5)
+
+link = browser.find_elements_by_class_name("js-cal-prev")[0]
+link.click()
+time.sleep(5)
+
+link = browser.find_elements_by_class_name("js-cal-prev")[0]
+link.click()
+time.sleep(5)
+
+link = browser.find_elements_by_class_name("js-cal-prev")[0]
+link.click()
+time.sleep(5)
+k = 0
+
+while k != 1:
+
+    html = browser.page_source
+    soup = BeautifulSoup(html, "html.parser")
+
+    com = soup.find_all('div', {"class": "normal-all-day"})
+
+    if len(com) > 0:
+        k = 1
+    else:
+        browser.refresh()
+        
+        time.sleep(10)
+
+        link = browser.find_elements_by_class_name("js-cal-prev")[0]
+        link.click()
+        time.sleep(5)
+
+        link = browser.find_elements_by_class_name("js-cal-prev")[0]
+        link.click()
+        time.sleep(5)
+
+        link = browser.find_elements_by_class_name("js-cal-prev")[0]
+        link.click()
+        time.sleep(5)
+
+        link = browser.find_elements_by_class_name("js-cal-prev")[0]
+        link.click()
+        time.sleep(5)
+
+        link = browser.find_elements_by_class_name("js-cal-prev")[0]
+        link.click()
+        time.sleep(5)
+
+        link = browser.find_elements_by_class_name("js-cal-prev")[0]
+        link.click()
+        time.sleep(5)
+
+        link = browser.find_elements_by_class_name("js-cal-prev")[0]
+        link.click()
+        time.sleep(5)
+
+        link = browser.find_elements_by_class_name("js-cal-prev")[0]
+        link.click()
+        time.sleep(5)
+
+        link = browser.find_elements_by_class_name("js-cal-prev")[0]
+        link.click()
+        time.sleep(5)
+
+        link = browser.find_elements_by_class_name("js-cal-prev")[0]
+        link.click()
+        time.sleep(5)
+
+
+html = browser.page_source
+soup = BeautifulSoup(html, "html.parser")
+
+info = soup.find_all("td", {"class": "calendar-day"})
+
+scrap(info, 'febrero', febrero)
+
+print(datos)
---
+++ scrap.py
@@ -0,0 +1,36 @@
+import time
+import re
+import subprocess
+
+x = 0
+
+while(x != 1):
+    tmp = 0
+    print('Elige el mes')
+
+    tmp = input()
+    if (tmp == '1'):
+        subprocess.Popen(['python', 'diciembre.py'])
+    if(tmp == '2'):
+        subprocess.Popen(['python', 'noviembre.py'])
+    if(tmp == '3'):
+        subprocess.Popen(['python', 'octubre.py'])
+    if(tmp == '4'):
+        subprocess.Popen(['python', 'septiembre.py'])
+    if(tmp == '5'):
+        subprocess.Popen(['python', 'agosto.py'])
+    if(tmp == '6'):
+        subprocess.Popen(['python', 'julio.py'])
+    if(tmp == '7'):
+        subprocess.Popen(['python', 'junio.py'])
+    if(tmp == '8'):
+        subprocess.Popen(['python', 'mayo.py'])
+    if(tmp == '9'):
+        subprocess.Popen(['python', 'abril.py'])
+    if(tmp == '10'):
+        subprocess.Popen(['python', 'marzo.py'])
+    if(tmp == '11'):
+        subprocess.Popen(['python', 'febrero.py'])
+    if(tmp == '12'):
+        subprocess.Popen(['python', 'enero.py'])
+    x = 1
